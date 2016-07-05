075dad7dfd6c03af7b81e6c919cb3d11d04ee56f
Joe Robles <joe.robles.pdj@gmail.com>
2016-07-05 13:34:17 -0500

    Preventivo entity generated

---
+++ src/AppBundle/Entity/Preventivo.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Preventivo
+ *
+ * @ORM\Table(name="preventivo")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PreventivoRepository")
+ */
+class Preventivo
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="enfermedad_preventivo", type="string", length=255)
+     */
+    private $enfermedadPreventivo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="descripcion_preventivo", type="text", nullable=true)
+     */
+    private $descripcionPreventivo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="grupo_riesgo_preventivo", type="string", length=255, nullable=true)
+     */
+    private $grupoRiesgoPreventivo;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set enfermedadPreventivo
+     *
+     * @param string $enfermedadPreventivo
+     * @return Preventivo
+     */
+    public function setEnfermedadPreventivo($enfermedadPreventivo)
+    {
+        $this->enfermedadPreventivo = $enfermedadPreventivo;
+
+        return $this;
+    }
+
+    /**
+     * Get enfermedadPreventivo
+     *
+     * @return string 
+     */
+    public function getEnfermedadPreventivo()
+    {
+        return $this->enfermedadPreventivo;
+    }
+
+    /**
+     * Set descripcionPreventivo
+     *
+     * @param string $descripcionPreventivo
+     * @return Preventivo
+     */
+    public function setDescripcionPreventivo($descripcionPreventivo)
+    {
+        $this->descripcionPreventivo = $descripcionPreventivo;
+
+        return $this;
+    }
+
+    /**
+     * Get descripcionPreventivo
+     *
+     * @return string 
+     */
+    public function getDescripcionPreventivo()
+    {
+        return $this->descripcionPreventivo;
+    }
+
+    /**
+     * Set grupoRiesgoPreventivo
+     *
+     * @param string $grupoRiesgoPreventivo
+     * @return Preventivo
+     */
+    public function setGrupoRiesgoPreventivo($grupoRiesgoPreventivo)
+    {
+        $this->grupoRiesgoPreventivo = $grupoRiesgoPreventivo;
+
+        return $this;
+    }
+
+    /**
+     * Get grupoRiesgoPreventivo
+     *
+     * @return string 
+     */
+    public function getGrupoRiesgoPreventivo()
+    {
+        return $this->grupoRiesgoPreventivo;
+    }
+}
